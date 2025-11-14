79cc6b6b58dae139e3b4f26472827b5f1f571316
kmtusher97 <kmtusher97@gmail.com>
2025-11-14 22:01:13 +0600

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import "./App.css";
+
 export function App() {
   return (
     <div className="">
       <h1>Hello world</h1>
-      <p>Welcome to your Vite + React + Tailwind CSS app!</p>
+      <p className="text">Welcome to your Vite + React + Tailwind CSS app!</p>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
+import "./index.css";
+
 const rootElement = document.getElementById("root");
 
 createRoot(rootElement).render(
