aec2a3f63af85bec6221f6ba1558fa8fcd0f1f13
kmtusher97 <kmtusher97@gmail.com>
2025-11-14 22:04:41 +0600

    configure tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ export function App() {
     <div className="">
       <h1>Hello world</h1>
       <p className="text">Welcome to your Vite + React + Tailwind CSS app!</p>
+
+      <div className="flex items-center justify-center mt-4">
+        <button className="btn bg-blue-500 text-white px-4 py-2 rounded">Click me</button>
+      </div>
     </div>
   );
 }
